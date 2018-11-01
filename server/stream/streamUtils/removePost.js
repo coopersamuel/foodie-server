a0b0d9558faccd793e4f6fca97333b560f4442b3
Samuel Cooper <cooper.samuel@outlook.com>
2018-10-31 21:41:53 -0400

    refactored delete post

--- server/stream/streamUtils/removePost.js
+++ server/stream/streamUtils/removePost.js
@@ -4,14 +4,9 @@ const User = mongoose.model('user');
 const Post = mongoose.model('post');
 
 module.exports = async (userId, postId) => {
-    await stream
-        .feed('user', userId)
-        .removeActivity({ foreignId: postId })
-        .then(async res => {
-            // Post deleted successfully, now remove from MongoDB
-            await Post.deleteOne({ _id: postId });
-        })
-        .catch(err => {
-            return err;
-        });
+    // Remove the post from MongoDB
+    await Post.deleteOne({ _id: postId });
+
+    // Remove the post from stream
+    await stream.feed('user', userId).removeActivity({ foreignId: postId });
 };
