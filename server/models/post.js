8c293a64ef5ad93503269b84227cd855d5e0ccb8
Samuel Cooper <cooper.samuel@outlook.com>
2019-03-03 23:19:08 -0500

    handling errors in remove post

--- server/models/post.js
+++ server/models/post.js
@@ -5,93 +5,99 @@ const Restaurant = mongoose.model('restaurant');
 
 // This is the Post schema for mongoose
 // This schema contains the data that will be stored in each Post record
-const PostSchema = new Schema({
+const PostSchema = new Schema(
+  {
     user: {
-        type: Schema.Types.ObjectId,
-        ref: 'user',
-        required: true,
-        autopopulate: {
-            select: [
-                'firstName',
-                'lastName',
-                'email',
-                'username'
-            ]
-        }
+      type: Schema.Types.ObjectId,
+      ref: 'user',
+      required: true,
+      autopopulate: {
+        select: ['firstName', 'lastName', 'email', 'username']
+      }
     },
     restaurant: {
-        type: Schema.Types.ObjectId,
-        ref: 'restaurant',
-        required: true,
-        autopopulate: {
-            select: [
-                'name',
-                'lat',
-                'long'
-            ]
-        }
+      type: Schema.Types.ObjectId,
+      ref: 'restaurant',
+      required: true,
+      autopopulate: {
+        select: ['name', 'lat', 'long']
+      }
     },
     review: {
-        type: Number,
-        required: true,
-        min: 1,
-        max: 5
+      type: Number,
+      required: true,
+      min: 1,
+      max: 5
     },
     comment: {
-        type: String,
-        trim: true,
-        default: null
+      type: String,
+      trim: true,
+      default: null
     },
     media: {
-        type: [String],
-        default: null
+      type: [String],
+      default: null
     },
     taggedUsers: {
-        type: [Schema.Types.ObjectId],
-        ref: 'user',
-        default: null,
-        autopopulate: {
-            select: [
-                'username'
-            ]
-        }
+      type: [Schema.Types.ObjectId],
+      ref: 'user',
+      default: null,
+      autopopulate: {
+        select: ['username']
+      }
     },
     hashtags: {
-        type: [String],
-        match: RegExp('^#','i'),
-        default: null
+      type: [String],
+      match: RegExp('^#', 'i'),
+      default: null
     }
-}, 
-{ 
+  },
+  {
     timestamps: true
-});
+  }
+);
 
 PostSchema.post('save', async function(doc, next) {
-    // Once a post is saved, update both the restaurant avgRating and ratingCount in the Restaurant collection
-    
-    // avgRating
-    await Restaurant.findById({ "_id": doc.restaurant }, async function(err, obj) { 
-         await Restaurant.findByIdAndUpdate(doc.restaurant, { $set: { avgRating: ((obj.toObject().ratingCount * obj.toObject().avgRating) + doc.review)/(obj.toObject().ratingCount + 1) } }
-    )});
-    
-    // ratingCount
-    await Restaurant.findByIdAndUpdate(doc.restaurant, { $inc: { ratingCount: 1 } });
-    next();
+  // Once a post is saved, update both the restaurant avgRating and ratingCount in the Restaurant collection
+
+  // avgRating
+  await Restaurant.findById({ _id: doc.restaurant }, async function(err, obj) {
+    await Restaurant.findByIdAndUpdate(doc.restaurant, {
+      $set: {
+        avgRating:
+          (obj.toObject().ratingCount * obj.toObject().avgRating + doc.review) /
+          (obj.toObject().ratingCount + 1)
+      }
+    });
+  });
+
+  // ratingCount
+  await Restaurant.findByIdAndUpdate(doc.restaurant, {
+    $inc: { ratingCount: 1 }
+  });
+  next();
 });
 
 PostSchema.post('remove', async function(doc, next) {
-    // When a post is removed, update both the restaurant avgRating and ratingCount in the Restaurant collection
-   
-    // avgRating 
-    await Restaurant.findById({ "_id": doc.restaurant }, async function(err, obj) { 
-        await Restaurant.findByIdAndUpdate(doc.restaurant, { $set: { avgRating: ((obj.toObject().ratingCount * obj.toObject().avgRating) - doc.review)/(obj.toObject().ratingCount - 1) } }
-   )});
+  // When a post is removed, update both the restaurant avgRating and ratingCount in the Restaurant collection
 
-    // ratingCount 
-    await Restaurant.findByIdAndUpdate(doc.restaurant, { $inc: { ratingCount: -1 } });
-    next();
+  // avgRating
+  await Restaurant.findById({ _id: doc.restaurant }, async function(err, obj) {
+    await Restaurant.findByIdAndUpdate(doc.restaurant, {
+      $set: {
+        avgRating:
+          (obj.toObject().ratingCount * obj.toObject().avgRating - doc.review) /
+          (obj.toObject().ratingCount - 1)
+      }
+    });
+  });
+
+  // ratingCount
+  await Restaurant.findByIdAndUpdate(doc.restaurant, {
+    $inc: { ratingCount: -1 }
+  });
+  next();
 });
-
 
 PostSchema.plugin(autopopulate);
 
--- server/stream/streamUtils/removePost.js
+++ server/stream/streamUtils/removePost.js
@@ -3,9 +3,15 @@ const mongoose = require('mongoose');
 const Post = mongoose.model('post');
 
 module.exports = async (userId, postId) => {
-    // Remove the post from MongoDB
-    await Post.deleteOne({ _id: postId });
+  // Remove the post from MongoDB
+  const post = await Post.findOne({ _id: postId })
+    .orFail()
+    .catch(error => {
+      throw new Error('Unable to locate this post');
+    });
 
-    // Remove the post from stream
-    await stream.feed('user', userId).removeActivity({ foreignId: postId });
+  await post.remove();
+
+  // Remove the post from stream
+  await stream.feed('user', userId).removeActivity({ foreignId: postId });
 };
